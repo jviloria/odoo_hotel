e381e4f87614dbb2a7df52e7b641959770fa05be
John W. Viloria Amaris <john.viloria.amaris@gmail.com>
2017-05-27 01:31:08 -0500

    [UPD] account_custom: se agrega el metodo _get_folio, para localizar el folio enlazado a la factura procesada

--- account_custom/report/account_invoice_report_list.py
+++ account_custom/report/account_invoice_report_list.py
@@ -14,8 +14,8 @@ _logger = logging.getLogger(__name__)
 
 class AccountInvoiceList(report_sxw.rml_parse):
 
-    def _get_sale_orders(self, form):
-        sale_obj = self.pool.get('sale.order')
+    def _get_account_invoices(self, form):
+        inv_obj = self.pool.get('account.invoice')
         user_obj = self.pool.get('res.users')
         data = []
         result = {}
@@ -31,109 +31,108 @@ class AccountInvoiceList(report_sxw.rml_parse):
         timestamp = user_tz.localize(timestamp).astimezone(pytz.utc)
         date_end = timestamp.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)
 
-        order_ids = sale_obj.search(self.cr, self.uid, [
-            ('date_order', '>=', date_start),
-            ('date_order', '<', date_end),
+        inv_ids = inv_obj.search(self.cr, self.uid, [
+            ('date_invoice', '>=', date_start),
+            ('date_invoice', '<', date_end),
             ('state', 'in', ['paid']),
             ('company_id', '=', company_id)
         ])
-        orders = sale_obj.browse(self.cr, self.uid, order_ids)
-        return orders
+        invoices = inv_obj.browse(self.cr, self.uid, inv_ids)
+        return invoices
 
-    def _get_product_ids(self, orders):
-        product_ids = []
-        proccesed = []
-        for order in orders:
-            for line in order.order_line:
-                if line.product_id.id not in proccesed:
-                    product_ids.append({
-                        'id': line.product_id.id,
-                        'product_name': line.name,
-                        'qty': 0
-                    })
-                    proccesed.append(line.product_id.id)
-        return product_ids
+    # def _get_product_ids(self, orders):
+    #     product_ids = []
+    #     proccesed = []
+    #     for order in orders:
+    #         for line in order.order_line:
+    #             if line.product_id.id not in proccesed:
+    #                 product_ids.append({
+    #                     'id': line.product_id.id,
+    #                     'product_name': line.name,
+    #                     'qty': 0
+    #                 })
+    #                 proccesed.append(line.product_id.id)
+    #     return product_ids
 
-    def _clear_product_ids(self, product_ids):
-        for product in product_ids:
-            product['qty'] = 0
+    # def _clear_product_ids(self, product_ids):
+    #     for product in product_ids:
+    #         product['qty'] = 0
 
-    def _get_customers(self, orders):
-        customers = []
-        proccesed = []
-        for order in orders:
-            if order.partner_id.id not in proccesed:
-                customers.append((order.partner_id.id, order.partner_id.name))
-                proccesed.append(order.partner_id.id)
-        return customers
+    # def _get_customers(self, orders):
+    #     customers = []
+    #     proccesed = []
+    #     for order in orders:
+    #         if order.partner_id.id not in proccesed:
+    #             customers.append((order.partner_id.id, order.partner_id.name))
+    #             proccesed.append(order.partner_id.id)
+    #     return customers
 
-    def _sum_product_qty(self, product_ids, line):
-        for product in product_ids:
-            if product['id'] == line.product_id.id:
-                product['qty'] += line.product_uom_qty
+    # def _sum_product_qty(self, product_ids, line):
+    #     for product in product_ids:
+    #         if product['id'] == line.product_id.id:
+    #             product['qty'] += line.product_uom_qty
 
-    def _sum_total_product_qty(self):
-        orders = self._get_orders()
-        footer = ['Total']
-        for product_id in self._get_product_list():
-            total_qty = 0
-            for order in orders:
-                for line in order.order_line:
-                    if line.product_id.id == product_id:
-                        total_qty += line.product_uom_qty
-            footer.append(total_qty)
-        self.total_product_qty = footer
-        return footer
+    # def _sum_total_product_qty(self):
+    #     orders = self._get_orders()
+    #     footer = ['Total']
+    #     for product_id in self._get_product_list():
+    #         total_qty = 0
+    #         for order in orders:
+    #             for line in order.order_line:
+    #                 if line.product_id.id == product_id:
+    #                     total_qty += line.product_uom_qty
+    #         footer.append(total_qty)
+    #     self.total_product_qty = footer
+    #     return footer
 
-    def _get_orders(self):
-        return self.orders
+    # def _get_orders(self):
+    #     return self.orders
 
-    def _get_product_list(self):
-        return self.product_list
+    # def _get_product_list(self):
+    #     return self.product_list
 
     def _get_header(self, form):
-        #orders = self._get_sale_orders(form)
-        return ['Partner Name','Folio','Room Number','Cash','Credit Card']
-        # proccesed = []
-        # for order in orders:
-        #     for line in order.order_line:
-        #         if line.product_id.id not in proccesed:
-        #             header.append(line.name[:20])
-        #             proccesed.append(line.product_id.id)
-        # self.product_list = proccesed
-        # self.orders = orders
-        # self._sum_total_product_qty()
-        #return header
+        return ['Partner Name','Folio','Room Number','Cash','Credit Card',
+            'Enterprise','Detail','Credit Card Brand']
+
+    def _get_folio(self, inv):
+        folio = False
+        folio_obj = self.pool.get('hotel.folio')
+        folio_ids = folio_obj.search(self.cr, self.uid, \
+                    [('invoice_status','=','invoiced'), \
+                     ('order_id.name','=',inv.origin)])
+        if folio_ids:
+            folio = folio_obj.browse(self.cr, self.uid, folio_ids)[0]
+        return folio
 
     def _compute_orders(self, form):
-        orders = self._get_sale_orders(form)
-        product_ids = self._get_product_ids(orders) #Plantilla de productos
-        self._clear_product_ids(product_ids)
-        customers = self._get_customers(orders)
-        report_lines = []
-        for customer_id, customer_name in customers:
-            customer_line = {'customer': customer_name}
-            for order in orders:
-                if order.partner_id.id == customer_id:
-                    for line in order.order_line:
-                        self._sum_product_qty(product_ids, line)
-            customer_line['product_ids'] = deepcopy(product_ids)
-            report_lines.append(customer_line)
-            self._clear_product_ids(product_ids)
-        return report_lines
+        invoices = self._get_account_invoices(form)
+        inv_list = []
+        for inv in invoices:
+            folio_number = ''
+            folio = self._get_folio(inv)
+            if folio:
+                folio_number = folio.name
+                data = {
+                    'partner': inv.partner_id.name,
+                    'folio': folio_number,
+                }
+                inv_list.append(data)
+        _logger.critical(inv_list)
+        return inv_list
 
-    def _get_date_start(self, form):
-        user_obj = self.pool.get('res.users')
-        data = []
-        result = {}
-        company_id = user_obj.browse(self.cr, self.uid, self.uid).company_id.id
-        user = self.pool['res.users'].browse(self.cr, self.uid, self.uid)
-        tz_name = user.tz or self.localcontext.get('tz') or 'UTC'
-        user_tz = pytz.timezone(tz_name)
-        between_dates = {}
-        timestamp = datetime.datetime.strptime(form['date_start'], tools.DEFAULT_SERVER_DATETIME_FORMAT)
-        timestamp = user_tz.localize(timestamp).astimezone(pytz.utc)
-        date_start = timestamp.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)
+    # def _get_date_start(self, form):
+    #     user_obj = self.pool.get('res.users')
+    #     data = []
+    #     result = {}
+    #     company_id = user_obj.browse(self.cr, self.uid, self.uid).company_id.id
+    #     user = self.pool['res.users'].browse(self.cr, self.uid, self.uid)
+    #     tz_name = user.tz or self.localcontext.get('tz') or 'UTC'
+    #     user_tz = pytz.timezone(tz_name)
+    #     between_dates = {}
+    #     timestamp = datetime.datetime.strptime(form['date_start'], tools.DEFAULT_SERVER_DATETIME_FORMAT)
+    #     timestamp = user_tz.localize(timestamp).astimezone(pytz.utc)
+    #     date_start = timestamp.strftime(tools.DEFAULT_SERVER_DATETIME_FORMAT)
 
     def __init__(self, cr, uid, name, context):
         super(AccountInvoiceList, self).__init__(cr, uid, name, context=context)
@@ -143,7 +142,7 @@ class AccountInvoiceList(report_sxw.rml_parse):
         self.orders = []
         self.localcontext.update({
             #'time': time,
-            #'compute_orders': self._compute_orders,
+            'compute_orders': self._compute_orders,
             'get_header': self._get_header,
             #'sum_total_product_qty': self._sum_total_product_qty
         })
